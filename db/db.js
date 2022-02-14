b5a2f037b65d3206f424cb38b97b9b2be5e54bf0
vivek mandal <vivek.mandal@vaptech.in>
2022-02-14 19:17:56 +0530

    db changed

--- db/db.js
+++ db/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//const url = "mongodb+srv://dev:devtest@cluster0.quj8h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const url = "mongodb://localhost:27017/blogpost";   
+const url = "mongodb+srv://dev:devtest@cluster0.quj8h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+//const url = "mongodb://localhost:27017/blogpost";   
 // Connect MongoDB at default port 27017.
 let mong = mongoose.connect(url, {
     useNewUrlParser: true,
